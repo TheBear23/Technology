7620bd8cba1b10c24daa6b0b5c64001f8e0ef464
TheBear23 <ntnict@yahoo.com>
2023-03-15 14:14:07 -0500

    Add project files.

---
+++ Computer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Technology
+{
+    public class Computer
+    {
+        public double Ram { get; set; }
+        public readonly double Screensize;
+        public string OS { get; set; }
+        public Computer(double ram, double screensize, string oS)
+        {
+            Ram = ram;
+            Screensize = screensize;
+            OS = OS;
+        }
+        public void PrintStatus()
+        {
+            Console.WriteLine($"RAM: {Ram}{Environment.NewLine}" + $"Screensize: {Screensize}{Environment.NewLine}" + $"OS: {OS}");
+        }
+        public void UpgradeRam(double Uram) 
+        {
+            Ram += Uram;
+        }
+    }
+}
